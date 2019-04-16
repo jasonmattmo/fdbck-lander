3f2c2ac5d0ae047b67f0088f3c68cf96b37aa411
Jason Booth <jason@mattmo.nl>
2019-04-16 18:03:21 +0200

    splash page structure

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+const blinkerTags = document.querySelectorAll('div.join-us a img.blink')
+const joinUsButton = document.querySelector('div.join-us a')
+
+blinkerTags.forEach(tag => {
+    tag.style.opacity = 0
+})
+
+joinUsButton.addEventListener('mouseover', function () {
+    blinkerTags.forEach(tag => {
+        tag.style.opacity = 1
+    })
+})
+
+joinUsButton.addEventListener('mouseout', function () {
+    blinkerTags.forEach(tag => {
+        tag.style.opacity = 0
+    })
+})
+
+
+
+
